57842e0253471aad640045272f636d24056acd64
odlmon <dmi3yosipov@gmail.com>
2020-11-22 20:07:10 +0300

    Feat: Add TestsGenerator library

---
+++ TestsGenerator/TestClass.cs
@@ -0,0 +1,14 @@
+﻿namespace TestsGenerator
+{
+    public class TestClass
+    {
+        public string FileName { get; private set; }
+        public string SourceCode { get; private set; }
+
+        public TestClass(string fileName, string code)
+        {
+            FileName = fileName;
+            SourceCode = code;
+        }
+    }
+}
